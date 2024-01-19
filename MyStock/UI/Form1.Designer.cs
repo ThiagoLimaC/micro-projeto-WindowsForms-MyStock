98016b573388da8a0fc629f0cca35b8d625a7b88
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2024-01-19 11:59:32 -0300

    inseri o código para exibir os dados do banco da datagridview

--- MyStock/UI/Form1.Designer.cs
+++ MyStock/UI/Form1.Designer.cs
@@ -44,23 +44,23 @@ namespace UI
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgProduto = new System.Windows.Forms.DataGridView();
             this.label10 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.button5 = new System.Windows.Forms.Button();
+            this.txtPesquisar = new System.Windows.Forms.TextBox();
+            this.btnPesquisar = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProduto)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -75,7 +75,7 @@ namespace UI
             this.panel1.ForeColor = System.Drawing.SystemColors.Desktop;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(267, 590);
+            this.panel1.Size = new System.Drawing.Size(267, 595);
             this.panel1.TabIndex = 0;
             // 
             // button3
@@ -258,19 +258,19 @@ namespace UI
             this.label6.TabIndex = 5;
             this.label6.Text = "Nome:";
             // 
-            // textBox1
+            // txtNome
             // 
-            this.textBox1.Location = new System.Drawing.Point(291, 227);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(304, 22);
-            this.textBox1.TabIndex = 6;
+            this.txtNome.Location = new System.Drawing.Point(291, 227);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(304, 22);
+            this.txtNome.TabIndex = 6;
             // 
-            // textBox2
+            // txtValor
             // 
-            this.textBox2.Location = new System.Drawing.Point(291, 289);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(304, 22);
-            this.textBox2.TabIndex = 8;
+            this.txtValor.Location = new System.Drawing.Point(291, 289);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(304, 22);
+            this.txtValor.TabIndex = 8;
             // 
             // label7
             // 
@@ -284,12 +284,12 @@ namespace UI
             this.label7.TabIndex = 7;
             this.label7.Text = "Valor:";
             // 
-            // textBox3
+            // txtCodigo
             // 
-            this.textBox3.Location = new System.Drawing.Point(291, 171);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(304, 22);
-            this.textBox3.TabIndex = 10;
+            this.txtCodigo.Location = new System.Drawing.Point(291, 171);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(304, 22);
+            this.txtCodigo.TabIndex = 10;
             // 
             // label8
             // 
@@ -315,13 +315,13 @@ namespace UI
             this.label9.TabIndex = 11;
             this.label9.Text = "Descrição:";
             // 
-            // textBox4
+            // txtDescricao
             // 
-            this.textBox4.Location = new System.Drawing.Point(291, 361);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(304, 140);
-            this.textBox4.TabIndex = 12;
+            this.txtDescricao.Location = new System.Drawing.Point(291, 361);
+            this.txtDescricao.Multiline = true;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(304, 140);
+            this.txtDescricao.TabIndex = 12;
             // 
             // button4
             // 
@@ -334,15 +334,15 @@ namespace UI
             this.button4.Text = "Salvar";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // dgProduto
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(624, 132);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(292, 432);
-            this.dataGridView1.TabIndex = 14;
+            this.dgProduto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProduto.Location = new System.Drawing.Point(624, 132);
+            this.dgProduto.Name = "dgProduto";
+            this.dgProduto.RowHeadersWidth = 51;
+            this.dgProduto.RowTemplate.Height = 24;
+            this.dgProduto.Size = new System.Drawing.Size(450, 432);
+            this.dgProduto.TabIndex = 14;
             // 
             // label10
             // 
@@ -352,45 +352,46 @@ namespace UI
             this.label10.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.label10.Location = new System.Drawing.Point(620, 25);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(297, 46);
+            this.label10.Size = new System.Drawing.Size(454, 46);
             this.label10.TabIndex = 15;
-            this.label10.Text = "Para pesquisar por um produto digite\r\nseu código no campo abaixo:\r\n";
+            this.label10.Text = "Para pesquisar por um produto digite seu código no cam-\r\npo abaixo:\r\n";
             // 
-            // textBox5
+            // txtPesquisar
             // 
-            this.textBox5.Location = new System.Drawing.Point(624, 83);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(198, 22);
-            this.textBox5.TabIndex = 16;
+            this.txtPesquisar.Location = new System.Drawing.Point(624, 78);
+            this.txtPesquisar.Multiline = true;
+            this.txtPesquisar.Name = "txtPesquisar";
+            this.txtPesquisar.Size = new System.Drawing.Size(357, 33);
+            this.txtPesquisar.TabIndex = 16;
             // 
-            // button5
+            // btnPesquisar
             // 
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.button5.Font = new System.Drawing.Font("Yu Gothic UI", 7.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(828, 83);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(88, 22);
-            this.button5.TabIndex = 17;
-            this.button5.Text = "Pesquisar";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnPesquisar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnPesquisar.Font = new System.Drawing.Font("Yu Gothic UI", 7.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesquisar.Location = new System.Drawing.Point(986, 78);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(88, 33);
+            this.btnPesquisar.TabIndex = 17;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
             // 
             // FrmProduto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(928, 590);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.textBox5);
+            this.ClientSize = new System.Drawing.Size(1088, 595);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.txtPesquisar);
             this.Controls.Add(this.label10);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgProduto);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txtDescricao);
             this.Controls.Add(this.label9);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtCodigo);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtValor);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtNome);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -406,7 +407,7 @@ namespace UI
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProduto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -428,18 +429,18 @@ namespace UI
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtValor;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtCodigo;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtDescricao;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgProduto;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox txtPesquisar;
+        private System.Windows.Forms.Button btnPesquisar;
     }
 }
 
--- MyStock/UI/Form1.cs
+++ MyStock/UI/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Business;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +20,39 @@ namespace UI
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            LoadAll();
+        }
 
+        private void LoadAll()
+        {
+            dgProduto.Rows.Clear();
+            dgProduto.AutoGenerateColumns = false;
+
+            dgProduto.ColumnCount = 4;
+            dgProduto.Columns[0].Name = "Codigo";
+            dgProduto.Columns[1].Name = "Nome";
+            dgProduto.Columns[2].Name = "Valor";
+            dgProduto.Columns[3].Name = "Descricao";
+
+            var rows = new List<string[]>();
+
+            foreach (Produto prod in new Produto().Todos())
+            {
+                string[] row1 = new string[]
+                {
+                        prod.CodigoProd,
+                        prod.Nome,
+                        "R$ " + prod.Valor.ToString(),
+                        prod.Descricao
+                };
+
+                rows.Add(row1);
+            }
+
+            foreach (string[] rowArray in rows)
+            {
+                dgProduto.Rows.Add(rowArray);
+            }
         }
     }
 }
