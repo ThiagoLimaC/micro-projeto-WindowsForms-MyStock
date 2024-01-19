bd3d88e5ab9fd426abdccfdc0f0078e331b0d5df
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2024-01-19 10:26:46 -0300

    criei e instanciei a classe Produto

---
+++ MyStock/Business/Produto.cs
@@ -0,0 +1,24 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class Produto : Base
+    {
+        [OpcoesBase(UsarNoBancoDeDados = true, ChavePrimaria = true, UsarParaBuscar = true)]
+        public string IdProd { get; set; }
+
+        [OpcoesBase(UsarNoBancoDeDados = true)]
+        public string Nome { get; set; }
+
+        [OpcoesBase(UsarNoBancoDeDados = true)]
+        public string Descricao { get; set; }
+
+        [OpcoesBase(UsarNoBancoDeDados = true)]
+        public decimal Valor { get; set; }
+    }
+}
